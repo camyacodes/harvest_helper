6377e358310b62c4a1a974a8e1a0459ff8ab4dd6
Camya Robinson <camyacodes@gmail.com>
2024-05-02 13:03:47 -0400

    Homepage & Header

---
+++ components/ui/header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="py-4 px-6 sm:px-12 flex items-center justify-between bg-beige text-[#3f4238]">
+      <div>
+        <a className="text-xl font-bold">Home</a>
+      </div>
+      <div>
+        <a className="text-xl font-bold">Login/Sign Up</a>
+      </div>
+    </header>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,52 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        beige: "#F0EAD2",
+        dark_green: "#3F4238",
+        light_green: "#DDE5B6",
+        dark_brown: "#6C584C",
+        light_brown: "#6C584C",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
